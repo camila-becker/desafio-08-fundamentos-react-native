a71582338574c3b41a018f80e8080d689af813aa
Camila Becker <camila.becker01@gmail.com>
2020-07-25 15:35:45 -0300

    Concluido o desafio

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const total = products.reduce((acc, product) => {
+      const productsSubtotal = product.price * product.quantity;
+      return acc + productsSubtotal;
+    }, 0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const totalItens = products.reduce((acc, product) => {
+      const productsQuantity = product.quantity;
+      return acc + productsQuantity;
+    }, 0);
+    return totalItens;
   }, [products]);
 
   return (
